4360efaa32ccc99eb659ed0d514b4451ec3efe84
Zahoor098Ahmed <132702248+Zahoor098Ahmed@users.noreply.github.com>
2024-02-25 15:40:00 +0500

    Chapter 1 done

---
+++ Chapter 1/apps.js
@@ -0,0 +1,8 @@
+
+//q1 alert
+
+alert('Hello')
+
+alert('Happy code')
+
+alert('JavaScript is the Programming Language for the Web. JavaScript can update and change both HTML and CSS. JavaScript can calculate, manipulate and validate data.')
